146712bf482de31eecb7a87ee1545c336f19f2c0
moni-varma <monivarma121@gmail.com>
2025-01-29 14:25:27 +0530

    Add files via upload

---
+++ query_3.sql
@@ -0,0 +1,12 @@
+-- identify the most common pizza size ordered
+
+SELECT 
+    pizzas.size,
+    COUNT(order_details.order_details_id) AS order_count
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY order_count DESC
+LIMIT 1;
