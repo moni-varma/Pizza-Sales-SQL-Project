cd3d974ffa89b3ffba72d3b07d0f5f5593c734d7
moni-varma <monivarma121@gmail.com>
2025-01-29 14:31:45 +0530

    Add files via upload

---
+++ query_8.sql
@@ -0,0 +1,13 @@
+-- deteremine the top 3 most ordered pizza types based on revenues
+SELECT 
+    pizza_types.name,
+    SUM(order_details.quantity * pizzas.price) AS revenue
+FROM
+    pizzas
+        JOIN
+    pizza_types ON pizzas.pizza_type_id = pizza_types.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY revenue DESC
+LIMIT 3;
